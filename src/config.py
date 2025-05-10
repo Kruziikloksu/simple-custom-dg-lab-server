e1b9f3d5ff8885c9fffd21b699276717634a6972
Kruziikloksu <623985209@qq.com>
2025-05-10 18:41:20 +0800

    Create config file

--- src/config.py
+++ src/config.py
@@ -1,10 +1,50 @@
+import os
 import socket
 import logging
+import sys
+import tomllib
 
-RUN_TEMP_CLIENT = True
+default_config = """
+[Misc]
+PORT = 4503
+RUN_TEMP_CLIENT = true
+HEARTBEAT_INTERVAL = 30
+LOG_TO_FILE = false
+"""
+
+
+def ensure_config():
+    config_path = get_config_path()
+    if not os.path.exists(config_path):
+        with open(config_path, "w") as f:
+            f.write(default_config)
+        print(f"Create config file: {config_path}")
+
+
+def get_config_path():
+    base_path = get_base_path()
+    return os.path.join(base_path, 'config.toml')
+
+
+def get_base_path():
+    if getattr(sys, 'frozen', False):
+        return os.path.dirname(sys.executable)
+    else:
+        return os.path.dirname(os.path.abspath(__file__))
+
+
+def load_toml_config():
+    config_path = get_config_path()
+    with open(config_path, "rb") as f:
+        return tomllib.load(f)
+
+
+ensure_config()
+toml_config = load_toml_config().get("Misc", {})
+RUN_TEMP_CLIENT = toml_config.get("RUN_TEMP_CLIENT", True)
 WS_SERVER_HOST = "0.0.0.0"
 WS_CLIENT_HOST = socket.gethostbyname(socket.gethostname())
-WS_SERVER_PORT = 4503
-HEARTBEAT_INTERVAL = 30
+WS_SERVER_PORT = toml_config.get("PORT", 4503)
+HEARTBEAT_INTERVAL = toml_config.get("HEARTBEAT_INTERVAL", 30)
 LOG_LEVEL = logging.DEBUG
-LOG_TO_FILE = True
+LOG_TO_FILE = toml_config.get("LOG_TO_FILE", False)
