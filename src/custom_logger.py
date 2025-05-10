73708c45b9d013eb35694700e9f075a6f379d549
Kruziikloksu <623985209@qq.com>
2025-05-10 22:58:33 +0800

    Fix logger

--- src/custom_logger.py
+++ src/custom_logger.py
@@ -1,16 +1,9 @@
 import logging
-from pathlib import Path
 import colorlog
 import os
 import config
 from datetime import datetime
 
-project_root = Path(__file__).resolve().parent.parent  # 假设代码位于src目录下
-log_directory = project_root / "logs"
-if not os.path.exists(log_directory):
-    os.makedirs(log_directory)
-
-log_file_path = os.path.join(log_directory, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
 
 logger = logging.getLogger("custom_logger")
 logger.setLevel(config.LOG_LEVEL)
@@ -36,10 +29,16 @@ console_handler.setFormatter(color_formatter)
 logger.addHandler(console_handler)
 
 if config.LOG_TO_FILE:
+    base_path = config.get_base_path()
+    log_directory = os.path.join(base_path, "logs")
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+    log_file_path = os.path.join(log_directory, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
     file_handler = logging.FileHandler(log_file_path, encoding="utf-8")
     file_handler.setLevel(config.LOG_LEVEL)
     file_handler.setFormatter(logging.Formatter(log_format))
     logger.addHandler(file_handler)
+
 
 def debug(msg): logger.debug(msg)
 def info(msg): logger.info(msg)
